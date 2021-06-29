fb375aaa262abff10a1ff9763a856fe054000b44
Hardik <hardik1509agrawal@gmail.com>
2021-06-29 19:50:29 +0530

    Database setup and Post Model

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 27, 2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'How I posted from future',
-        'content': 'Second post content',
-        'date_posted': 'June 27, 2031'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
