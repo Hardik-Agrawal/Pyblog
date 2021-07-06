e4dad58b61d805c69f2b81638dd8ac25c68d26fc
Hardik <hardik1509agrawal@gmail.com>
2021-07-06 13:46:28 +0530

    reset password

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -136,3 +136,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# Mailing service Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('Email_User')
+EMAIL_HOST_PASSWORD = os.environ.get('Email_Pass')
