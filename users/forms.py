ffcdc79cc99183919cda5673693ba51830cd35de
Hardik <hardik1509agrawal@gmail.com>
2021-07-09 22:15:01 +0530

    removed profile

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+
 
 
 class UserRegisterForm(UserCreationForm):
@@ -19,10 +19,5 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image']
 
 
